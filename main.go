3513b8b9b93b799c0a277a13eeca397f6e80038f
Andrey Kislyak <deepdivenow@gmail.com>
2021-06-03 13:39:05 +0300

    Add ssh opts

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -10,6 +11,7 @@ import (
 	"sort"
 	"strconv"
 	"strings"
+	"text/template"
 
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/compute/v1"
 	ycsdk "github.com/yandex-cloud/go-sdk"
@@ -17,7 +19,24 @@ import (
 
 var (
 	instancePerPage int64 = 256
+	args            argsT
+	errNat          = errors.New("Nat IP not found")
 )
+
+type argsT struct {
+	List        bool
+	Host        string
+	Ssh         bool
+	SshUser     string
+	SshPort     int
+	SshNatGroup string
+}
+
+type sshConf struct {
+	User   string
+	GW     string
+	GWPort int
+}
 
 type ansibleGroup struct {
 	Hosts    []string               `json:"hosts,omitempty"`
@@ -29,22 +48,70 @@ type ansibleInventory map[string]ansibleGroup
 type ansibleVars map[string]string
 
 func main() {
-	argList := flag.Bool("list", false, "List inventory")
-	argHost := flag.String("host", "", "Get Host vars")
+	flag.BoolVar(&args.List, "list", false, "List inventory")
+	flag.StringVar(&args.Host, "host", "", "Get Host vars")
+	flag.BoolVar(&args.Ssh, "ssh", false, "Get ssh.conf")
+	flag.StringVar(&args.SshUser, "ssh-user", "cloud-user", "Set user for ssh.conf")
+	flag.StringVar(&args.SshNatGroup, "ssh-nat-group", "nat", "Set nat group for ssh.conf")
+	flag.IntVar(&args.SshPort, "ssh-port", 22, "Set GW port for ssh.conf")
 	flag.Parse()
 
-	if *argList {
-		ansibleList()
-	} else if len(*argHost) > 0 {
-		ansibleHost(*argHost)
+	if args.List {
+		ai, err := newAnsibleInventory()
+		if err != nil {
+			log.Fatal(err)
+		}
+		ai.print()
+	} else if args.Ssh {
+		getSshConf()
+	} else if len(args.Host) > 0 {
+		ansibleHost(args.Host)
 	} else {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
-
+}
+func getSshConf() {
+	ai, err := newAnsibleInventory()
+	if err != nil {
+		log.Fatal(err)
+	}
+	natList, ok := ai[args.SshNatGroup]
+	if !ok {
+		log.Fatal(errNat)
+	}
+	if len(natList.Hosts) < 1 {
+		log.Fatal(errNat)
+	}
+	natHost := natList.Hosts[0]
+	natHostIp,ok := ai["_meta"].HostVars[natHost]["public_address"]
+	if !ok {
+		log.Fatal(errNat)
+	}
+	conf := sshConf{
+		User:   args.SshUser,
+		GW:     natHostIp,
+		GWPort: args.SshPort,
+	}
+	t, err := template.New("todos").Parse("Host *\n  User={{ .User }}\n  ProxyCommand=ssh -q -W %h:%p {{ .User }}@{{ .GW }} -p{{ .GWPort }}\n")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = t.Execute(os.Stdout, conf)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-func ansibleList() {
+func (ai *ansibleInventory) print() {
+	prepareBytes, err := json.MarshalIndent(ai, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(string(prepareBytes))
+}
+
+func newAnsibleInventory() (ansibleInventory, error) {
 	envLabels := []string{"YC_TOKEN", "FOLDER_ID", "WORKSPACE"}
 	envs := map[string]string{}
 	for _, e := range envLabels {
@@ -61,11 +128,11 @@ func ansibleList() {
 		Credentials: ycsdk.OAuthToken(envs["YC_TOKEN"]),
 	})
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	instances, err := getInstances(ctx, sdk, envs["FOLDER_ID"])
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	ansibleInventory := ansibleInventory{}
 	ansibleMeta := map[string]ansibleVars{}
@@ -83,7 +150,7 @@ func ansibleList() {
 		sort.Strings(groupAllItem.Hosts)
 		ansibleInventory["all"] = groupAllItem
 		ansibleMeta[i.Name] = ansibleVars{"ansible_host": i.GetNetworkInterfaces()[0].GetPrimaryV4Address().Address}
-		if len(i.GetNetworkInterfaces()[0].GetPrimaryV4Address().GetOneToOneNat().GetAddress()) > 0{
+		if len(i.GetNetworkInterfaces()[0].GetPrimaryV4Address().GetOneToOneNat().GetAddress()) > 0 {
 			ansibleMeta[i.Name]["public_address"] = i.GetNetworkInterfaces()[0].GetPrimaryV4Address().GetOneToOneNat().GetAddress()
 		}
 
@@ -98,22 +165,19 @@ func ansibleList() {
 		ansibleInventory[group] = groupItem
 	}
 
-	for k,v := range ansibleInventory {
-		if k == "all" || k == "_meta" { continue }
-		for i,h := range v.Hosts{
-			ansibleMeta[h][fmt.Sprintf("tf_group_%s_id",k)]=strconv.Itoa(i)
+	for k, v := range ansibleInventory {
+		if k == "all" || k == "_meta" {
+			continue
+		}
+		for i, h := range v.Hosts {
+			ansibleMeta[h][fmt.Sprintf("tf_group_%s_id", k)] = strconv.Itoa(i)
 		}
 	}
 
 	metaGroup := ansibleGroup{}
 	metaGroup.HostVars = ansibleMeta
 	ansibleInventory["_meta"] = metaGroup
-
-	prepareBytes, err := json.MarshalIndent(ansibleInventory, "", "  ")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Print(string(prepareBytes))
+	return ansibleInventory, nil
 }
 
 func ansibleHost(h string) {
